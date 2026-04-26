ee8c536163a043bf049f45bab1d9eb74dc4e88a6
JuanjoRestrepo <restrepojuanjo@gmail.com>
2026-04-25 21:02:07 -0500

    feat: - Defined a new model `Task` with fields `title`, `description`, and a foreign key relationship to `Project`. - Created corresponding migration files for both `Project` and `Task` models.

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    # Relationship to Project thru ForeignKey - CASCADE means if a project is deleted, all related tasks will also be deleted
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
